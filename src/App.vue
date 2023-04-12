e06cb54a6fb8bf992b5817edd6ea404f61537929
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-12 15:44:54 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>This is my first Vite setup</h1>
+  <strong>Hello World!!!</strong>
+</template>
+
+<style></style>
